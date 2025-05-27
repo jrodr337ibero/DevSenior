8d8eaa17b6bcd30aa316c7daa372076b2575ec88
Jhonn.SR <jrodriguez@corvatech.com>
2025-05-26 21:54:53 -0500

    Ajustes en el database

--- RETO_4-5/model/database/conexion_db.py
+++ RETO_4-5/model/database/conexion_db.py
@@ -1,20 +1,16 @@
 import mysql.connector
 from mysql.connector import Error
 
-
 class ConexionDB:
-    
     __instancia = None
-    
+
     def __new__(cls):
-        
         if cls.__instancia is None:
             cls.__instancia = super(ConexionDB, cls).__new__(cls)
         return cls.__instancia
-    
+
     def __init__(self):
-        
-        if not hasattr(self, '_conexion'):
+        if not hasattr(self, '_conexion') or not self._conexion.is_connected():
             try:
                 self._conexion = mysql.connector.connect(
                     host='localhost',
@@ -23,10 +19,12 @@ class ConexionDB:
                     database='Academia'
                 )
                 if self._conexion.is_connected():
-                    print('conexion establecida')
+                    print('Conexión establecida')
             except Error as ex:
-                print('Error de conexion a la base de datos')
-    
-    def     get_conexion(self):
-        return self._conexion if self._conexion.is_connected() else None
-        
+                print('Error de conexión a la base de datos:', ex)
+                self._conexion = None
+
+    def get_conexion(self):
+        if self._conexion and self._conexion.is_connected():
+            return self._conexion
+        return None
